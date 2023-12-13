f9d47c5d31869ca566115bc88b1d0eace3818dac
WolfmosRioja <sergiolmos_@hotmail.com>
2023-12-13 12:07:56 +0100

    Update Enemy.test.js

--- __tests__/Enemy.test.js
+++ __tests__/Enemy.test.js
@@ -1,6 +1,16 @@
-import Enemy from './Enemy';
+const {Enemy} = require('../script')
 
 describe('Enemy', () => {
+    beforeEach(() => {
+        document.getElementById = jest.fn().mockImplementation((id) => {
+          if (id === 'passwordInput') {
+            return {
+              addEventListener: jest.fn(),
+            };
+          }
+          return null;
+        });
+      });
   const mockGame = { width: 800, height: 600 };
 
   test('should be initially marked as free', () => {
