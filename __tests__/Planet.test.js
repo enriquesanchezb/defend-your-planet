73d6c9d602f853ea4c2ba9a10a036882fa35ecf6
WolfmosRioja <sergiolmos_@hotmail.com>
2023-12-13 12:08:20 +0100

    Update Planet.test.js

--- __tests__/Planet.test.js
+++ __tests__/Planet.test.js
@@ -1,6 +1,16 @@
-import Planet from './Planet';
+const {Planet} = require('../script')
 
 describe('Planet', () => {
+    beforeEach(() => {
+        document.getElementById = jest.fn().mockImplementation((id) => {
+          if (id === 'passwordInput') {
+            return {
+              addEventListener: jest.fn(),
+            };
+          }
+          return null;
+        });
+      });
   test('should be instantiated with correct properties', () => {
     const mockGame = { width: 800, height: 600, debug: false };
     const planet = new Planet(mockGame);
